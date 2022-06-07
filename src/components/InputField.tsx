9a7a529e8fdb5d0ad5abe9d8610aa9d878370199
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-07 15:52:54 +0300

    Made some changes to my input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import "./styles.css"
 
 interface Props{
-    todo:string,
-    setTodo:React.Dispatch<React.SetStateAction<string>>
+    todo:string;
+    setTodo:React.Dispatch<React.SetStateAction<string>>;
 }
 
-const InputField = ({todo, setTodo}:Props) => {
+const InputField:React.FC<Props> = ({todo, setTodo}:Props) => {
     return (
         <form action="" className="input">
-            <input type="input" className="input_box" placeholder='Enter a task'/>
+            <input type="input" className="input_box" placeholder='Enter a task' 
+            value={todo} 
+            onChange={
+                (e) => setTodo(e.target.value)
+            }/>
             <button className="input_submit" type='submit'>Go</button>
         </form>
     )
