81985348324ab9148aa800cb2f4e8d21375a75bf
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-07 15:13:29 +0300

    Got rid of the code I was writing to create an app

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,10 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-let printNameFunction: (name:string) => void;
-
-function printName(name:string) {
-  console.log(name);
-}
-
-interface PersonInterface {
-  name: string,
-  age: number,
-  isAdult: boolean
-}
-
-class John implements PersonInterface {
-  name = "Chickenmwings";
-  age = 28;
-  isAdult = true
-};
-
-printName("Shapoopie");
-
-// type Person={
-//   name: string,
-//   age?: number
-// };
-
-// let person: Person = {
-//   name: "Nemo",
-//   age: 12
-// };
-
-// let lotsOfPeople: Person[];
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <span className="heading">Taskify</span>
     </div>
   );
 }
