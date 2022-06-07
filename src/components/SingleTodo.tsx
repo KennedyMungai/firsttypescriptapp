1ffc6048f6dca8bdc9cb807fcd309618525d68b6
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-07 16:58:38 +0300

    Added teh actual logic

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Todo } from '../model'
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
+import { MdDone } from 'react-icons/md'
+import "./styles.css"
 
-const SingleTodo = () => {
+type Props = {
+    todo: Todo,
+    todos: Todo[],
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const SingleTodo = ({todo, todos, setTodos}: Props) => {
     return (
-        <div>SingleTodo</div>
+        <form className='todos_single'>
+            <span className="todos_single--text">
+                {todo.todo}
+            </span>
+            <div className="">
+                <span className="icon">
+                    <AiFillEdit />
+                </span>
+                <span className="icon">
+                    <AiFillDelete />
+                </span>
+                <span className="icon">
+                    <MdDone />
+                </span>
+            </div>
+        </form>
     )
 }
 
