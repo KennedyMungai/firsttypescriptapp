34aa7a3ef0d67ad85d3efbdd3e023f693c61da33
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-07 15:43:39 +0300

    defined todo and setTodo

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import "./styles.css"
 
-const InputField = () => {
+interface Props{
+    todo:string,
+    setTodo:React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField = ({todo, setTodo}:Props) => {
     return (
         <form action="" className="input">
             <input type="input" className="input_box" placeholder='Enter a task'/>
